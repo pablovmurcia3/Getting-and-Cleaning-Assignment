49d65d079b65ca81a7207b9cdb527a6b5b19395c
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-21 09:05:22 -0500

    script

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,9 @@
                                 # Run_analysis #                                                                                                                   
 ################################################################################
 
-        ## Part 0 reading and constructing the data sets ##
+
+              ## Part 0 reading and constructing the data sets ##
+
 
 # 0.1 read the elements of the test, train and features data
 
@@ -18,7 +20,6 @@ y_train <- read.table("./UCI HAR dataset/train/y_train.txt")
 
 # features
 features <- read.table("./UCI HAR dataset/features.txt")[,2]
-features <-as.data.frame(features)
 
 # 0.2 construct the test and train databases
 
@@ -31,7 +32,7 @@ testDB <- cbind(subject_test, y_test, X_test)
 trainDB <- cbind(subject_train, y_train, X_train)
 
 
-                        ## Part 1 Merging the data sets ##
+                            ## Part 1 Merging the data sets ##
 
 # merge datasets
 mergeDB <- rbind(testDB,trainDB)
@@ -44,17 +45,40 @@ colnames(mergeDB) <- c("subject", "activity", features)
                         ## Part 2 Extracting only the measurements ##
                           ## on the mean and standard deviation ##
 
-mergeDBsub <- mergeDB[,grepl("subject|activity|mean[()]|std()", colnames(mergeDB))] 
+mergeDBsub <- mergeDB[, grepl("subject|activity|mean[()]|std()", colnames(mergeDB))] 
 
-# Part 3 -- Using descriptive activity names to name the activities in the data set
+
+                        ## Part 3 Using descriptive activity names ##
+                         ## to name the activities in the data set ##
+
 
 mergeDBsub$activity <- as.factor(mergeDBsub$activity)
 
 levels(mergeDBsub$activity) <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS" , "SITTING",
                  "STANDING", "LAYING")
 
-# Part 4 -- label the data set with descriptive variable names.
+mergeDBsub$activity <- tolower(mergeDBsub$activity)
 
 
+                     ## Part 4: label the data set with descriptive ## 
+                                ## variable names ## 
 
-# Part 5 -- creating data set with the average of each variable for each activity and each subject.
+
+# the data already have names, so in this step I limited to clean the names
+
+colnames(mergeDBsub) <- gsub("-",".", colnames(mergeDBsub))
+
+colnames(mergeDBsub) <- gsub("()","", fixed = TRUE, colnames(mergeDBsub))
+
+
+                    ## Part 5: creating data set with the average of ##
+                  ## each variable for each activity and each subject ##
+
+library(dplyr)
+
+finalTidy <- mergeDBsub %>% group_by(subject, activity) %>% summarize_all(mean)
+
+
+write.table(finalTidy, file = "finalTidy.txt", row.name=FALSE)
+
+table <- read.table("finalTidy.txt", header = TRUE)
