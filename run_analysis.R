215eccfe9a5e4d14820c6b4ccdf0f42d97c23b34
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-21 14:55:28 -0500

    script

--- run_analysis.R
+++ run_analysis.R
@@ -3,11 +3,14 @@
 ################################################################################
 
 
-              ## Part 0 reading and constructing the data sets ##
+           ## Part 0 downloading, reading and constructing the datasets ##
 
+# download the data with the following code
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="UCI HAR dataset.zip",method="curl") # it takes some minutes depending on your internet connection
+unzip(zipfile="UCI HAR dataset.zip")
 
-# 0.1 read the elements of the test, train and features data
-
+# read test, train and features data
 # test data
 X_test <- read.table("./UCI HAR dataset/test/X_test.txt")
 subject_test <-read.table("./UCI HAR dataset/test/subject_test.txt")
@@ -21,18 +24,18 @@ y_train <- read.table("./UCI HAR dataset/train/y_train.txt")
 # features
 features <- read.table("./UCI HAR dataset/features.txt")[,2]
 
-# 0.2 construct the test and train databases
+# construct the test and train databases
 
-# test database
+# test dataset
 
 testDB <- cbind(subject_test, y_test, X_test)
 
-# train database
+# train dataset
 
 trainDB <- cbind(subject_train, y_train, X_train)
 
 
-                            ## PART 1 Merging the data sets ##
+                            ## PART 1 Merging the datasets ##
 
 # merge datasets
 mergeDB <- rbind(testDB,trainDB)
@@ -64,7 +67,7 @@ mergeDBsub$activity <- tolower(mergeDBsub$activity)
                               ## variable names ## 
 
 
-# the data already have names, so in this step I limited to clean the names
+# the data already have column names, so I limited to clean the names
 
 colnames(mergeDBsub) <- gsub("-",".", colnames(mergeDBsub))
 
@@ -74,13 +77,12 @@ colnames(mergeDBsub) <- gsub("()","", fixed = TRUE, colnames(mergeDBsub))
                  ## PART 5: creating data set with the average of ##
                ## each variable for each activity and each subject ##
 
-library(dplyr)
+library(dplyr) # load the dplyr package in order to manipulate the merged data set
 
 finalTidy <- mergeDBsub %>% group_by(subject, activity) %>% summarize_all(mean)
 
 
+
 write.table(finalTidy, file = "finalTidy.txt", row.name=FALSE)
 
-table <- read.table("finalTidy.txt", header = TRUE)
-################### Don´t forget to set Header = TRUE in the read.table command :)
-z
+##### Don´t forget to set Header = TRUE in the read.table command :) ###########
